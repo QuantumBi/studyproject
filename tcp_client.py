2c48cbc51149ec741322ea965a430fa0b9f27eb2
Kamil Haidarov <ckamil.haidarov@yandex.ru>
2025-01-03 18:20:06 +0300

    v 1.1

--- tcp_client.py
+++ tcp_client.py
@@ -6,7 +6,7 @@ max_size = 1000
 print("starting the cleint\n")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(address)
-kek = 0
+
 while True:
     sms = input("-->")
     if sms == "exit":
